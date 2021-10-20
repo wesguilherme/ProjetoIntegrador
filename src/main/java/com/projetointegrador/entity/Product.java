fd6f6c7a5d9264c0f566c318c2bc38530a6238d3
anacfsoares <anacarol.fs@hotmail.com>
2021-10-20 18:51:35 -0300

    Criando classes

---
+++ src/main/java/com/projetointegrador/entity/Product.java
@@ -0,0 +1,27 @@
+package com.projetointegrador.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    //@Id
+    private String productId;
+    private String name;
+    private String description;
+
+    public Product(String productId, String name, String description) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " \"productId\": \"" + productId + "\"" +
+                ", \"name\":\"" + name + "\"" +
+                ", \"description\":\"" + description + "\"" +
+                "}";
+    }
+}
