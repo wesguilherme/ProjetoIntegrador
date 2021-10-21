5c129a09b87078a39b64489826d6727e778340aa
rarosario <rafael.rosario@mercadolivre.com>
2021-10-21 12:13:57 -0300

    Correção das classes

--- src/main/java/com/projetointegrador/entity/BatchStock.java
+++ src/main/java/com/projetointegrador/entity/BatchStock.java
@@ -2,16 +2,12 @@ package com.projetointegrador.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
-@Entity
 @Data
 public class BatchStock {
 
-    @Id
     private String batchId;
     private LocalDate dueDate;
     private LocalDateTime manufacturingTime;
@@ -21,16 +17,16 @@ public class BatchStock {
     private Float minimumTemperature;
     private String currentTemperature;
 
-    @OneToMany
+//    @OneToMany
     private Section sectionId;
 
-    @OneToMany(mappedBy = "batchStock", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<Product> product;
+//    @OneToMany
+    private Product product;
 
     public BatchStock() {
     }
 
-    public BatchStock(String batchId, LocalDate dueDate, LocalDateTime manufacturingTime, LocalDate manufacturingDate, Integer currentQuantity, Integer initialQuantity, Float minimumTemperature, String currentTemperature, Section sectionId, List<Product> product) {
+    public BatchStock(String batchId, LocalDate dueDate, LocalDateTime manufacturingTime, LocalDate manufacturingDate, Integer currentQuantity, Integer initialQuantity, Float minimumTemperature, String currentTemperature, Section sectionId, Product product) {
         this.batchId = batchId;
         this.dueDate = dueDate;
         this.manufacturingTime = manufacturingTime;
