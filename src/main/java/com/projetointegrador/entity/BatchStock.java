a05edf8cdeb417ff355b18059d7a09fc324fd676
rarosario <rafael.rosario@mercadolivre.com>
2021-10-21 15:56:59 -0300

    Modificação de classe 2

--- src/main/java/com/projetointegrador/entity/BatchStock.java
+++ src/main/java/com/projetointegrador/entity/BatchStock.java
@@ -2,7 +2,7 @@ package com.projetointegrador.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,7 +22,7 @@ public class BatchStock {
     private String currentTemperature;
 
     @OneToMany
-    private Section sectionId;
+    private Section section;
 
     @OneToMany(mappedBy = "batchStock", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Product> product;
@@ -30,7 +30,7 @@ public class BatchStock {
     public BatchStock() {
     }
 
-    public BatchStock(String batchId, LocalDate dueDate, LocalDateTime manufacturingTime, LocalDate manufacturingDate, Integer currentQuantity, Integer initialQuantity, Float minimumTemperature, String currentTemperature, Section sectionId, List<Product> product) {
+    public BatchStock(String batchId, LocalDate dueDate, LocalDateTime manufacturingTime, LocalDate manufacturingDate, Integer currentQuantity, Integer initialQuantity, Float minimumTemperature, String currentTemperature, Section section, List<Product> product) {
         this.batchId = batchId;
         this.dueDate = dueDate;
         this.manufacturingTime = manufacturingTime;
@@ -39,7 +39,7 @@ public class BatchStock {
         this.initialQuantity = initialQuantity;
         this.minimumTemperature = minimumTemperature;
         this.currentTemperature = currentTemperature;
-        this.sectionId = sectionId;
+        this.section = section;
         this.product = product;
     }
 
@@ -47,7 +47,7 @@ public class BatchStock {
     public String toString() {
         return "{" +
                 "\"batchid\":" + batchId +
-                ", \"sectionid\":\"" + sectionId + "\"" +
+                ", \"section\":\"" + section + "\"" +
                 ", \"duedate\":\"" + dueDate + "\"" +
                 ", \"manufacturingtime\":\"" + manufacturingTime + "\"" +
                 ", \"manufacturingdate\":\"" + manufacturingDate + "\"" +
