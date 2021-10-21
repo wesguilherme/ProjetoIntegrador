c9251ec9276a07de4b984ddd8240705093da84e6
rarosario <rafael.rosario@mercadolivre.com>
2021-10-21 12:13:57 -0300

    Correção das classes

--- src/main/java/com/projetointegrador/entity/Seller.java
+++ src/main/java/com/projetointegrador/entity/Seller.java
@@ -1,64 +1,29 @@
 package com.projetointegrador.entity;
-
 import lombok.Data;
 
-import javax.persistence.*;
-import javax.swing.text.MaskFormatter;
-import java.text.ParseException;
-
-@Entity
 @Data
-public class Seller{
+public class Seller extends Person{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long sellerId;
-    private String cpf;
-    private String name;
+    private String sellerId;
+    private Integer personId;
+    private Integer addressId;
 
-    @Embedded
-    private Address address;
-
-    public Seller(){
-
-    }
-
-    public Seller(Long sellerId, String cpf, String name, Address address) {
+    public Seller(String cpf, String name, Integer address, String sellerId, Integer personId, Integer addressId) {
+        super(cpf, name, address);
         this.sellerId = sellerId;
-        this.cpf = cpf;
-        this.name = name;
-        this.address = address;
-    }
-
-    public String validaCpf(String cpf) throws ParseException {
-        MaskFormatter mf = new MaskFormatter("###.###.###-##");
-        mf.setValueContainsLiteralCharacters(false);
-        return mf.valueToString(cpf);
-    }
-
-    public void setSellerId(Long sellerId) {
-        this.sellerId = sellerId;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
+        this.personId = personId;
+        this.addressId = addressId;
     }
 
     @Override
     public String toString() {
-        return "{" +
-                " \"name\":" + name + "\"" +
-                ", \"cpf\":\"" + cpf + "\"" +
-                ", \"sellerId\":" + sellerId +
-                ", \"address\":" + address +
+        return "Seller{" +
+                " \"nome\":" + super.getName() + "\"" +
+                ", \"cpf\":\"" + super.getCpf() + "\"" +
+                ", \"endereço\":\"" + super.getAddressId() + "\"" +
+                "sellerId='" + sellerId + "\"" +
+                ", personId=" + personId + "\"" +
+                ", addressId=" + addressId + "\"" +
                 '}';
     }
 }
