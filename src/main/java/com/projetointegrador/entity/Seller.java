981798479f870a53a9826aba47600f9bb0338b8c
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-10-21 15:00:56 -0300

    Alteraçao das classes Person.java, Seller.java e Representative.java

--- src/main/java/com/projetointegrador/entity/Seller.java
+++ src/main/java/com/projetointegrador/entity/Seller.java
@@ -1,29 +1,35 @@
 package com.projetointegrador.entity;
 import lombok.Data;
 
+import javax.persistence.*;
+import java.util.List;
+
 @Data
+@Entity
 public class Seller extends Person{
 
+    @Id
     private String sellerId;
-    private Integer personId;
-    private Integer addressId;
 
-    public Seller(String cpf, String name, Integer address, String sellerId, Integer personId, Integer addressId) {
-        super(cpf, name, address);
+    @OneToMany(mappedBy = "Seller", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Address> Addresses;
+
+    public Seller(){
+    }
+
+    public Seller(String cpf, String name, String sellerId, List<Address> addresses) {
+        super(cpf, name);
         this.sellerId = sellerId;
-        this.personId = personId;
-        this.addressId = addressId;
+        Addresses = addresses;
     }
 
-    @Override
-    public String toString() {
-        return "Seller{" +
-                " \"nome\":" + super.getName() + "\"" +
-                ", \"cpf\":\"" + super.getCpf() + "\"" +
-                ", \"endereço\":\"" + super.getAddressId() + "\"" +
-                "sellerId='" + sellerId + "\"" +
-                ", personId=" + personId + "\"" +
-                ", addressId=" + addressId + "\"" +
-                '}';
-    }
+    // @Override
+//    public String toString() {
+//        return "Seller{" +
+//                " \"name\":" + super.getName() + "\"" +
+//                ", \"cpf\":\"" + super.getCpf() + "\"" +
+//                ", \"sellerId\":" + sellerId +
+//                ", \"address\":" + address +
+//                '}';
+//    }
 }
