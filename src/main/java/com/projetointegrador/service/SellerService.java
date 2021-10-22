d0ba602140e89b3c6e1c116ddfe936f50af0a111
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-10-22 16:40:15 -0300

    Alteraçao das classes SellerPersistence.java e SellerService.java

--- src/main/java/com/projetointegrador/service/SellerService.java
+++ src/main/java/com/projetointegrador/service/SellerService.java
@@ -1,77 +1,44 @@
 package com.projetointegrador.service;
 
-import com.projetointegrador.dao.SellerPersistence;
+import com.projetointegrador.repository.SellerPersistence;
 import com.projetointegrador.entity.Seller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+@Service
 public class SellerService {
 
+    @Autowired
     private SellerPersistence sellerPersistence;
 
+    public SellerService(){
+    }
 
     public SellerService(SellerPersistence sellerPersistence) {
         this.sellerPersistence = sellerPersistence;
     }
 
     /**
-     *
-     * @return
+     * @param cpf - é esperado o parametro cpf do vendedor
+     * @return - retorna a verificaçao de duplicidade do código
      * @author Grupo 5 - Tester Wesley
      */
-    private Long codigoUnico() {
-        return ThreadLocalRandom.current().nextLong(100, 9999);
-    }
-
-    /**
-     *
-     * @param seller
-     * @author Grupo 5 - Tester Wesley
-     */
-    public void salva(Seller seller) {
-        if(seller.getSellerId()==null || seller.getSellerId().isEmpty()) {
-            seller.getSellerId();
-            sellerPersistence.cadastrar(seller);
-        }else{
-            Seller sellerExists = sellerPersistence.get(String.valueOf(seller.getSellerId()));
-            if(sellerExists == null){
-                throw new RuntimeException("Algo de muito errado aconteceu");
-            }
-            sellerExists.setCpf(seller.getCpf());
-            sellerExists.setName(seller.getName());
-            sellerExists.setAddresses(seller.getAddresses());
-            sellerPersistence.atualiza(sellerExists);
+    private boolean codigoNaoUtilizado(String cpf) {
+        Seller sellerExistente = sellerPersistence.findByCpf(cpf);
+        if (sellerExistente == null) {
+            return false;
         }
+        return true;
     }
 
-//    /**
-//     *
-//     * @return uma lista de sellers ordenada (em ordem crescente) pelo nome
-//     */
-//    public List<Seller> listagem(){
-//        List<Seller> lista = sellerPersistence.lista();
-//        lista.sort((Seller v1, Seller v2) -> v1.getName().compareToIgnoreCase(v2.getName()));
-//        return lista;
-//    }
-
-    /**
-     *
-     * @param codigo
-     * @return
-     * @author Grupo 5 - Tester Wesley
-     */
-    public Seller obtem(String codigo) {
-        return sellerPersistence.get(codigo);
-    }
-
-    /**
-     *
-     * @param codigo
-     * @author Grupo 5 - Tester Wesley
-     */
-    public void deleta(String codigo) {
-        sellerPersistence.exclui(codigo);
+    public Seller cadastrar(Seller seller) throws IOException {
+        if (codigoNaoUtilizado(seller.getCpf())) {
+            return sellerPersistence.save(seller);
+        } else {
+            throw new RuntimeException("Cpf já utilizado");
+        }
     }
 }
