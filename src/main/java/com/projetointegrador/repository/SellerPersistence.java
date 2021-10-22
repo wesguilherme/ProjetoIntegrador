1b9e5fcc07d0b2b568a07a64b6e6bbb75cb1b7e6
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-10-22 16:40:15 -0300

    Alteraçao das classes SellerPersistence.java e SellerService.java

---
+++ src/main/java/com/projetointegrador/repository/SellerPersistence.java
@@ -0,0 +1,12 @@
+package com.projetointegrador.repository;
+
+import com.projetointegrador.entity.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerPersistence extends JpaRepository<Seller, Long> {
+
+    Seller findByCpf(String cpf);
+
+}
