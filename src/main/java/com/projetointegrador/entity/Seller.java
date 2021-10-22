7d8168ac44b335604838b1a397c8bbe6f036e665
anacfsoares <anacarol.fs@hotmail.com>
2021-10-22 10:50:53 -0300

    Refatorando classes e pom

--- src/main/java/com/projetointegrador/entity/Seller.java
+++ src/main/java/com/projetointegrador/entity/Seller.java
@@ -2,26 +2,38 @@ package com.projetointegrador.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
 import java.util.List;
 
 @Data
 @Entity
-public class Seller extends Person{
+public class Seller{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long sellerId;
+    private String cpf;
+    private String name;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Address> Addresses;
 
     public Seller(){
+
     }
 
-    public Seller(String cpf, String name, Long sellerId, List<Address> addresses) {
-        super(cpf, name);
+    public Seller(Long sellerId, String cpf, String name, List<Address> addresses) {
         this.sellerId = sellerId;
+        this.cpf = cpf;
+        this.name = name;
         Addresses = addresses;
+    }
+
+    public String validaCpf(String cpf) throws ParseException {
+        MaskFormatter mf = new MaskFormatter("###.###.###-##");
+        mf.setValueContainsLiteralCharacters(false);
+        return mf.valueToString(cpf);
     }
 
     // @Override
