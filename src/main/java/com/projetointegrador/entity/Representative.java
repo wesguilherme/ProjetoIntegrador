d4707cbe9d3be65becbdcab84a1a4be915fbbffb
anacfsoares <anacarol.fs@hotmail.com>
2021-10-22 10:50:53 -0300

    Refatorando classes e pom

--- src/main/java/com/projetointegrador/entity/Representative.java
+++ src/main/java/com/projetointegrador/entity/Representative.java
@@ -3,15 +3,19 @@ package com.projetointegrador.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
 import java.util.List;
 
 @Data
 @Entity
-public class Representative extends Person{
+public class Representative{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long representativeId;
+    private String cpf;
+    private String name;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Address> Addresses;
@@ -20,13 +24,20 @@ public class Representative extends Person{
 
     }
 
-    public Representative(String cpf, String name, Long representativeId, List<Address> addresses) {
-        super(cpf, name);
+    public Representative(Long representativeId, String cpf, String name, List<Address> addresses) {
         this.representativeId = representativeId;
+        this.cpf = cpf;
+        this.name = name;
         Addresses = addresses;
     }
 
-//    @Override
+    public String validaCpf(String cpf) throws ParseException {
+        MaskFormatter mf = new MaskFormatter("###.###.###-##");
+        mf.setValueContainsLiteralCharacters(false);
+        return mf.valueToString(cpf);
+    }
+
+    //    @Override
 //    public String toString() {
 //        return "Seller{" +
 //                " \"nome\":" + super.getName() + "\"" +
