5244ef009b045fea43bdece8ce71142592ea6b84
rarosario <rafael.rosario@mercadolivre.com>
2021-10-23 16:31:47 -0300

    Persistencia no ProductSeller

--- src/main/java/com/projetointegrador/repository/ProductSellerPersistence.java
+++ src/main/java/com/projetointegrador/repository/ProductSellerPersistence.java
@@ -1,11 +1,9 @@
 package com.projetointegrador.repository;
 
-
-import com.projetointegrador.entity.InboundOrder;
+import com.projetointegrador.entity.ProductSeller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InboudOrderPersistence extends JpaRepository<InboundOrder, Long> {
-
+public interface ProductSellerPersistence extends JpaRepository<ProductSeller, Long> {
 }
