d47d7603811b34933b68b4510300ff4234231367
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-10-23 15:12:33 -0300

    Criaçao do controller SectionController.java e alteraçao de classes

--- src/main/java/com/projetointegrador/controller/SectionController.java
+++ src/main/java/com/projetointegrador/controller/SectionController.java
@@ -1,6 +1,5 @@
 package com.projetointegrador.controller;
 
-import com.projetointegrador.dto.SectionDto;
 import com.projetointegrador.entity.Section;
 import com.projetointegrador.service.SectionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,27 +12,22 @@ import java.net.URI;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/api/v1/")
+@RequestMapping(value = "/section")
 public class SectionController {
 
     @Autowired
     private SectionService sectionService;
 
-    @PostMapping(value = "/section/insert")
-    public ResponseEntity<Section> insert(@RequestBody SectionDto sectionDto, UriComponentsBuilder uriBuilder) throws IOException {
-        Section sectionCadastrado = sectionService.insert(sectionDto);
+    @PostMapping(value = "/cadastrar")
+    public ResponseEntity<Section> cadastrar(@RequestBody Section section, UriComponentsBuilder uriBuilder) throws IOException {
+        Section sectionCadastrado = sectionService.cadastrar(section);
 
-        URI uri = uriBuilder.path("/section/search/{id}").buildAndExpand(sectionCadastrado.getSectionCode()).toUri();
+        URI uri = uriBuilder.path("/section/buscar/{id}").buildAndExpand(sectionCadastrado.getSectionId()).toUri();
         return ResponseEntity.created(uri).body(sectionCadastrado);
     }
 
-    @GetMapping("/buscar/{code}")
-    public Optional<Section> getSectionById(@PathVariable("id") String code) {
-        return sectionService.getSectionById(code);
-    }
-
-    @GetMapping("/verificar/{code}")
-    public boolean verifyValidSection(@PathVariable("code") String code) {
-        return sectionService.verifyValidSection(code);
+    @GetMapping("/buscar/{id}")
+    public Optional<Section> buscaSetorPorId(@PathVariable("id") Long id) {
+        return sectionService.buscaSetorPorId(id);
     }
 }
--- src/main/java/com/projetointegrador/service/SectionService.java
+++ src/main/java/com/projetointegrador/service/SectionService.java
@@ -1,9 +1,6 @@
 package com.projetointegrador.service;
 
-import com.projetointegrador.dto.SectionDto;
-import com.projetointegrador.entity.Representative;
 import com.projetointegrador.entity.Section;
-import com.projetointegrador.entity.Warehouse;
 import com.projetointegrador.repository.SectionPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,85 +10,63 @@ import java.util.Optional;
 @Service
 public class SectionService {
 
+
     @Autowired
     private SectionPersistence sectionPersistence;
 
-    @Autowired
-    private RepresentativeService representativeService;
-
-    @Autowired
-    private WarehouseService warehouseService;
-
     public SectionService() {
-
     }
 
-    /**
-     * @param sectionPersistence - é esperado um parâmetro do tipo sectionPersistence para injeção de dependência
-     * @author - Grupo 5 - Tester Wesley
-     */
     public SectionService(SectionPersistence sectionPersistence) {
         this.sectionPersistence = sectionPersistence;
     }
 
-    /**
-     * @param sectionDto - é esperado um objeto do tipo sectionDto
-     * @return - retorna section cadastrado na lista
-     * @author - Grupo 5 - Tester Wesley
-     */
-    public Section insert(SectionDto sectionDto) {
-        Section section = convert(sectionDto);
 
-        if (section.getRepresentative() != null && section.getWarehouse() != null) {
+    /**
+     * @param sectionId - é esperado o parametro cpf do vendedor
+     * @return - retorna a verificaçao de duplicidade do código
+     * @author Grupo 5 - Tester Wesley
+     */
+    private boolean codigoNaoUtilizado(Long sectionId) {
+        Optional<Section> sectionExistente = sectionPersistence.findById(sectionId);
+        if (sectionExistente == null) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public Section cadastrar(Section section) {
+        if (codigoNaoUtilizado(section.getSectionId())) {
             return sectionPersistence.save(section);
+        } else {
+            throw new RuntimeException("Setor já utilizado");
         }
 
-        throw new RuntimeException("Representante ou armazém não existe!");
-
     }
 
-    /**
-     * @param code - é esperado o parametro code da section
-     * @return - retorna a section através do Id
-     * @author - Grupo 5 - Tester Wesley
-     */
-    public Optional<Section> getSectionById(String code) {
-        return sectionPersistence.findBySectionCode(code);
+//    public Section cadastrar(Section section, Double usedSpace, Double availableSpace, Double volume) {
+//        if (codigoNaoUtilizado(section.getSectionId()) && espacoDisponivel(volume, section)) {
+//            var totalUtilizado = section.getUsedSpace()+volume;
+//            section.setUsedSpace(totalUtilizado);
+//            return sectionPersistence.save(section);
+//        } else {
+//            throw new RuntimeException("Setor já utilizado");
+//        }
+//
+//    }
+
+    public Optional<Section> buscaSetorPorId(Long sectionId) {
+        return sectionPersistence.findById(sectionId);
     }
 
-    /**
-     * @param code - é esperado o parametro code da section
-     * @return - retorna a verificação tru ou false sobre a validade da section
-     * @author - Grupo 5 - Tester Wesley
-     */
-    public boolean verifyValidSection(String code) {
-        Optional<Section> verifyValidSection = sectionPersistence.findBySectionCode(code);
-        if (verifyValidSection.isEmpty()) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * @param sectionDto - é esperado um objeto do tipo sectionDto
-     * @return - retorna a section com os dados de representative e warehouse
-     * @author - Grupo 5 - Tester Wesley
-     */
-    public Section convert(SectionDto sectionDto) {
-        Section section = new Section();
-        section.setSectionCode(sectionDto.getSectionCode());
-        section.setSectionType(sectionDto.getSectionType());
-        section.setTotalCapacity(sectionDto.getTotalCapacity());
-        section.setUsedSpace(sectionDto.getUsedSpace());
-
-        Representative r = representativeService.getByIdRepresentative(sectionDto.getRepresentativeId());
-        Warehouse w = warehouseService.getByCode(sectionDto.getWarehouseCode());
-
-        section.setRepresentative(r);
-        section.setWarehouse(w);
-
-        return section;
-    }
-
-    // falta fazer o verificaEspacoDisponivel
+//    public boolean espacoDisponivel(Long sectionId){
+//        if (section.getTotalCapacity() > volume) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//        Section section = buscaSetorPorId(sectionId);
+//        if (section.get)
+//    }
 }
