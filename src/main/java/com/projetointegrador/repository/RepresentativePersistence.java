355a49f9966ca30488ae969d219ecf55235e21d8
anacfsoares <anacarol.fs@hotmail.com>
2021-10-25 16:47:34 -0300

    Persistindo Section e padronizando nome das classes

--- src/main/java/com/projetointegrador/repository/RepresentativePersistence.java
+++ src/main/java/com/projetointegrador/repository/RepresentativePersistence.java
@@ -4,8 +4,10 @@ import com.projetointegrador.entity.Representative;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RepresentativePersistence extends JpaRepository<Representative, Long> {
 
-    Representative findByCpf(String cpf);
+    Optional<Representative> findByCpf(String cpf);
 }
