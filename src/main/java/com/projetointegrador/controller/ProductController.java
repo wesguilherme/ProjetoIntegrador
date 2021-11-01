18241ae5ab0111fbe1e1fb82f89537d149c9416b
rarosario <rafael.rosario@mercadolivre.com>
2021-11-01 19:01:27 -0300

    Desenvolvimento das autenticações do Requisito 2

--- src/main/java/com/projetointegrador/controller/ProductController.java
+++ src/main/java/com/projetointegrador/controller/ProductController.java
@@ -10,14 +10,13 @@ import com.projetointegrador.service.ProductService;
 import com.projetointegrador.service.PurchaseOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/product")
--- src/main/java/com/projetointegrador/repository/InboundOrderPersistence.java
+++ src/main/java/com/projetointegrador/repository/InboundOrderPersistence.java
@@ -2,8 +2,12 @@ package com.projetointegrador.repository;
 
 
 import com.projetointegrador.entity.InboundOrder;
+import com.projetointegrador.entity.Type;
+import com.projetointegrador.entity.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface InboundOrderPersistence extends JpaRepository<InboundOrder, Long> {
