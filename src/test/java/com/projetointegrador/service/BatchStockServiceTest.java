e0a5ca71743e92791249eece330ac34d45855e6c
allozano33 <alessandro.lozano@mercadolivre.com>
2021-11-09 18:58:55 -0300

    Atualizacao classes de Teste BatchStockServiceTest

--- src/test/java/com/projetointegrador/service/BatchStockServiceTest.java
+++ src/test/java/com/projetointegrador/service/BatchStockServiceTest.java
@@ -3,8 +3,8 @@ package com.projetointegrador.service;
 import com.projetointegrador.entity.*;
 import com.projetointegrador.repository.BatchStockPersistence;
 import com.projetointegrador.repository.ProductSellerPersistence;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.access.method.P;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -42,4 +43,48 @@ public class BatchStockServiceTest {
         List<BatchStock> batchStock1 = batchStockService.getBatchStockByProductSeller(productSeller);
         assertNotNull(batchStock1.get(0).getBatchStockId());
     }
-}
+
+    @Test
+    void shouldNotgetBatchStockByProductSeller() {
+        BatchStockPersistence mock = mock(BatchStockPersistence.class);
+
+        Type type = new Type(1L, "RF", "REFRIGERADOS");
+        Product product = new Product("MLB-123", "Uva", "Caixa de Uva", type);
+
+
+        BatchStockService batchStockService = new BatchStockService(mock);
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+            batchStockService.getBatchStockByProductSeller(ProductSeller.builder().build());
+        });
+
+        assertEquals("Não existe batchStock para esse produto.", exception.getMessage());
+    }
+
+//    @Test
+//    void shouldverifyProductInBatchStock() {
+//        BatchStockPersistence mock = mock(BatchStockPersistence.class);
+//
+//        Type type = new Type(1L, "RF", "REFRIGERADOS");
+//        Address address = new Address("rua goias", "44", "99999-000", "sp", "sp", "cs");
+//        Seller seller = new Seller(1L, "111.222.333-44", "Rafael", address);
+//        Product product = new Product("MLB-123", "Uva", "Caixa de Uva", type);
+//
+//        ProductSeller productSeller = new ProductSeller(1L, 10.0, 5.0, 1.0, seller, product, new BigDecimal("20"));
+//
+//        when(mock.findById(1L)).thenReturn(Optional.of(productSeller));
+//
+//        ProductSellerService productSellerService = new ProductSellerService(mock);
+//        ProductSeller productSeller1 = productSellerService.getProductSeller(1L);
+//        assertNotNull(productSeller1.getProductSellerId());
+//
+//        List<BatchStock> batchStockList = new ArrayList<>();
+//        BatchStock batchStock = new BatchStock(1L, LocalDate.now(),LocalDateTime.now(), LocalDate.now(), 50, 100, 34F, "16", 5L, productSeller);
+//        batchStockList.add(batchStock);
+//        when(mock.findByProductSeller(any(ProductSeller.class))).thenReturn(batchStockList);
+//
+//        BatchStockService batchStockService = new BatchStockService(mock);
+//        List<BatchStock> batchStock1 = batchStockService.getBatchStockByProductSeller(productSeller);
+//        assertNotNull(batchStock1.get(0).getBatchStockId());
+    }
+
+
--- src/test/java/com/projetointegrador/service/BuyerServiceTest.java
+++ src/test/java/com/projetointegrador/service/BuyerServiceTest.java
@@ -3,15 +3,17 @@ package com.projetointegrador.service;
 
 import com.projetointegrador.entity.Address;
 import com.projetointegrador.entity.Buyer;
+import com.projetointegrador.entity.Product;
+import com.projetointegrador.entity.Type;
 import com.projetointegrador.repository.BuyerPersistence;
+import com.projetointegrador.repository.ProductSellerPersistence;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,10 +21,10 @@ import static org.mockito.Mockito.when;
 public class BuyerServiceTest {
 
     @Test
-    void mustInsertSeller() {
+    void mustInsertBuyer ( ) {
         BuyerPersistence mock1 = mock(BuyerPersistence.class);
         BuyerService mock = mock(BuyerService.class);
-        Address address = new Address("rua goias", "44","99999-000", "sp", "sp", "cs");
+        Address address = new Address("rua goias", "44", "99999-000", "sp", "sp", "cs");
         Buyer buyer = new Buyer(1L, "33377799955", "Wes", address);
 
         when(mock.insert(any(Buyer.class))).thenReturn(buyer);
@@ -33,10 +35,10 @@ public class BuyerServiceTest {
     }
 
     @Test
-    void mustNotInsertSeller() {
+    void mustNotInsertBuyer ( ) {
         BuyerPersistence mock1 = mock(BuyerPersistence.class);
         String cpf = "33399977788";
-        Address address = new Address("rua goias", "44","99999-000", "sp", "sp", "cs");
+        Address address = new Address("rua goias", "44", "99999-000", "sp", "sp", "cs");
         Buyer buyer = new Buyer(1L, "33399977788", "Wes", address);
         when(mock1.findByCpf(cpf)).thenReturn(buyer);
 
@@ -51,14 +53,30 @@ public class BuyerServiceTest {
     }
 
     @Test
-    void mustgetByIdSeller(){
+    void shouldGetByIdBuyer ( ) {
         BuyerPersistence mock1 = mock(BuyerPersistence.class);
-        Address address = new Address("rua goias", "44","99999-000", "sp", "sp", "cs");
-        Optional<Buyer> buyer= Optional.of(new Buyer(1L, "33399977788", "Wes", address));
+        Address address = new Address("rua goias", "44", "99999-000", "sp", "sp", "cs");
+        Optional<Buyer> buyer = Optional.of(new Buyer(1L, "33399977788", "Wes", address));
         when(mock1.findById(1L)).thenReturn(buyer);
 
         BuyerService buyerService = new BuyerService(mock1);
         Buyer buyer1 = buyerService.getByIdBuyer(1L);
         assertNotNull(buyer1.getBuyerId());
+
+    }
+
+    @Test
+    void shouldNotGetProductSellerByProduto ( ) {
+        BuyerPersistence mock = mock(BuyerPersistence.class);
+
+        Type type = new Type(1L, "RF", "REFRIGERADOS");
+        Product product = new Product("MLB-123", "Uva", "Caixa de Uva", type);
+
+
+        BuyerService buyerService = new BuyerService(mock);
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, ( ) -> {
+            buyerService.getByIdBuyer(1l);
+        });
+        assertEquals("Não existe comprador cadastrado!", exception.getMessage());
     }
 }
