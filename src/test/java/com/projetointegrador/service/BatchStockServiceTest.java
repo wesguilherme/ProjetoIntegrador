67d3c9253c64f5a6d5adb2fff3b4781d7acf5672
allozano33 <alessandro.lozano@mercadolivre.com>
2021-11-10 10:35:49 -0300

    Alteraçoes da Classe Teste BatchStockServiceTest

--- src/test/java/com/projetointegrador/service/BatchStockServiceTest.java
+++ src/test/java/com/projetointegrador/service/BatchStockServiceTest.java
@@ -22,9 +22,8 @@ import static org.mockito.Mockito.when;
 public class BatchStockServiceTest {
 
 
-
     @Test
-    void shouldgetBatchStockByProductSeller() {
+    void shouldgetBatchStockByProductSeller ( ) {
         BatchStockPersistence mock1 = mock(BatchStockPersistence.class);
 
         Type type = new Type(1L, "RF", "REFRIGERADOS");
@@ -35,7 +34,7 @@ public class BatchStockServiceTest {
         ProductSeller productSeller = new ProductSeller(1L, 10.0, 5.0, 1.0, seller, product, new BigDecimal("20"));
 
         List<BatchStock> batchStockList = new ArrayList<>();
-        BatchStock batchStock = new BatchStock(1L, LocalDate.now(),LocalDateTime.now(), LocalDate.now(), 50, 100, 34F, "16", 5L, productSeller);
+        BatchStock batchStock = new BatchStock(1L, LocalDate.now(), LocalDateTime.now(), LocalDate.now(), 50, 100, 34F, "16", 5L, productSeller);
         batchStockList.add(batchStock);
         when(mock1.findByProductSeller(any(ProductSeller.class))).thenReturn(batchStockList);
 
@@ -45,7 +44,7 @@ public class BatchStockServiceTest {
     }
 
     @Test
-    void shouldNotgetBatchStockByProductSeller() {
+    void shouldNotgetBatchStockByProductSeller ( ) {
         BatchStockPersistence mock = mock(BatchStockPersistence.class);
 
         Type type = new Type(1L, "RF", "REFRIGERADOS");
@@ -53,38 +52,10 @@ public class BatchStockServiceTest {
 
 
         BatchStockService batchStockService = new BatchStockService(mock);
-        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, ( ) -> {
             batchStockService.getBatchStockByProductSeller(ProductSeller.builder().build());
         });
 
         assertEquals("Não existe batchStock para esse produto.", exception.getMessage());
     }
-
-//    @Test
-//    void shouldverifyProductInBatchStock() {
-//        BatchStockPersistence mock = mock(BatchStockPersistence.class);
-//
-//        Type type = new Type(1L, "RF", "REFRIGERADOS");
-//        Address address = new Address("rua goias", "44", "99999-000", "sp", "sp", "cs");
-//        Seller seller = new Seller(1L, "111.222.333-44", "Rafael", address);
-//        Product product = new Product("MLB-123", "Uva", "Caixa de Uva", type);
-//
-//        ProductSeller productSeller = new ProductSeller(1L, 10.0, 5.0, 1.0, seller, product, new BigDecimal("20"));
-//
-//        when(mock.findById(1L)).thenReturn(Optional.of(productSeller));
-//
-//        ProductSellerService productSellerService = new ProductSellerService(mock);
-//        ProductSeller productSeller1 = productSellerService.getProductSeller(1L);
-//        assertNotNull(productSeller1.getProductSellerId());
-//
-//        List<BatchStock> batchStockList = new ArrayList<>();
-//        BatchStock batchStock = new BatchStock(1L, LocalDate.now(),LocalDateTime.now(), LocalDate.now(), 50, 100, 34F, "16", 5L, productSeller);
-//        batchStockList.add(batchStock);
-//        when(mock.findByProductSeller(any(ProductSeller.class))).thenReturn(batchStockList);
-//
-//        BatchStockService batchStockService = new BatchStockService(mock);
-//        List<BatchStock> batchStock1 = batchStockService.getBatchStockByProductSeller(productSeller);
-//        assertNotNull(batchStock1.get(0).getBatchStockId());
-    }
-
-
+}
